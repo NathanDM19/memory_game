02d48f65d87d926580775157f220cfcdd0941417
NathanDM19 <38101742+NathanDM19@users.noreply.github.com>
2018-04-07 17:36:36 +1000

    Added functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
-cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped "+cardOne);
-console.log("User flipped "+cardTwo);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
 	}
 }
+var flipCard = function (cardId) {
+	console.log("User flipped "+cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
